ab84b45b98634e0e002501cd0b7511bd4c95eb30
Artem Kasianov <zhonayer2017@gmail.com>
2024-06-02 20:24:39 +0300

    test 6

--- src/services/getGameBySlug.js
+++ src/services/getGameBySlug.js
@@ -6,37 +6,6 @@ const getGameBySlug = async(gameSlug) => {
   return prisma.game.findUnique({
     where: {
       slug: gameSlug
-    },
-    include: {
-      company: {
-        select: { name: true }
-      },
-      genres: {
-        select: {
-          Genre: {
-            select: { genre: true }
-          }
-        }
-      },
-      comments: {
-        include: {
-          user: {
-            select: {
-              username: true,
-            },
-          },
-        },
-      },
-      userScore: {
-        select: {
-          markOne: true,
-          markTwo: true,
-          markThree: true,
-          markFour: true,
-          markFive: true
-        }
-      },
-      requirements: true
     }
   });
 }
