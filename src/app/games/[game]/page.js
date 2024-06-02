2deb8047f511e502c1b39a35a54731cdfe95f784
Artem Kasianov <zhonayer2017@gmail.com>
2024-06-02 19:39:25 +0300

    test 2

--- src/app/games/[game]/page.js
+++ src/app/games/[game]/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PageComponent from './PageComponent';
 import helloworld from '@/controllers/helloworld';
 
-async function Gamepage() {
-  const data = await helloworld();
+async function Gamepage({ params }) {
+  const data = await helloworld(params.slug);
 
   return (
     <PageComponent data={data} />
--- src/controllers/helloworld.js
+++ src/controllers/helloworld.js
@@ -1,5 +1,7 @@
-const helloworld = async() => {
-  return Promise.resolve('DATA');
+const helloworld = async(slug) => {
+  const data = await Promise.resolve('DATA with slug: ' + data);
+
+  return data;
 }
 
 export default helloworld;
